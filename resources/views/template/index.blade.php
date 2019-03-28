fbdb4a5f98d32a03fff84ec43ff0e113a0908f9c
Nguyễn Đức Anh <ducanh.nguyen@sotatek.com>
2019-03-29 00:56:19 +0700

    first_commit

---
+++ resources/views/template/index.blade.php
@@ -0,0 +1,469 @@
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+	<title>Draw Education Category Bootstrap Responsive Website Template | Home :: W3layouts</title>
+	<!-- Meta tag Keywords -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="UTF-8" />
+	<meta name="keywords" content="Draw Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+	<script>
+		addEventListener("load", function () {
+			setTimeout(hideURLbar, 0);
+		}, false);
+
+		function hideURLbar() {
+			window.scrollTo(0, 1);
+		}
+	</script>
+	<!-- //Meta tag Keywords -->
+
+	<!-- Custom-Files -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<!-- Bootstrap-Core-CSS -->
+	<link href="css/css_slider.css" type="text/css" rel="stylesheet" media="all">
+	<!-- banner slider -->
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+	<!-- Style-CSS -->
+	<link href="css/font-awesome.min.css" rel="stylesheet">
+	<!-- Font-Awesome-Icons-CSS -->
+	<!-- //Custom-Files -->
+
+	<!-- Web-Fonts -->
+	<link href="//fonts.googleapis.com/css?family=Sarabun:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i"
+	 rel="stylesheet">
+	<link href="//fonts.googleapis.com/css?family=Crimson+Text:400,400i,600,600i,700,700i" rel="stylesheet">
+	<!-- //Web-Fonts -->
+</head>
+
+<body>
+	<!-- main banner -->
+	<div class="main-top" id="home">
+		<!-- header -->
+		<header>
+			<div class="container-fluid">
+				<div class="header d-md-flex justify-content-between align-items-center py-3 px-xl-5 px-lg-3 px-2">
+					<!-- logo -->
+					<div id="logo">
+						<h1><a href="index.html">Draw</a></h1>
+					</div>
+					<!-- //logo -->
+					<!-- nav -->
+					<div class="nav_w3ls">
+						<nav>
+							<label for="drop" class="toggle">Menu</label>
+							<input type="checkbox" id="drop" />
+							<ul class="menu">
+								<li><a href="index.html" class="active">Home</a></li>
+								<li><a href="about.html">About Us</a></li>
+								<li><a href="gallery.html">Gallery</a></li>
+								<li>
+									<!-- First Tier Drop Down -->
+									<label for="drop-2" class="toggle toogle-2">Dropdown <span class="fa fa-angle-down" aria-hidden="true"></span>
+									</label>
+									<a href="#">Dropdown <span class="fa fa-angle-down" aria-hidden="true"></span></a>
+									<input type="checkbox" id="drop-2" />
+									<ul>
+										<li><a href="#services" class="drop-text">Services</a></li>
+										<li><a href="about.html" class="drop-text">Team</a></li>
+										<li><a href="about.html" class="drop-text">Statistics</a></li>
+										<li><a href="#testi" class="drop-text">Testimonials</a></li>
+										<li><a href="#sub" class="drop-text">Subscribe Form</a></li>
+									</ul>
+								</li>
+								<li><a href="contact.html">Contact Us</a></li>
+							</ul>
+						</nav>
+					</div>
+					<!-- //nav -->
+				</div>
+			</div>
+		</header>
+		<!-- //header -->
+
+		<!-- banner -->
+		<div class="banner_w3lspvt">
+			<div class="csslider infinity" id="slider1">
+				<input type="radio" name="slides" checked="checked" id="slides_1" />
+				<input type="radio" name="slides" id="slides_2" />
+				<input type="radio" name="slides" id="slides_3" />
+				<ul class="banner_slide_bg">
+					<li>
+						<div class="container">
+							<div class="w3ls_banner_txt text-center">
+								<h3 class="w3ls_pvt-title text-wh text-uppercase let">W3layouts Website</h3>
+								<p class="text-li">New HTML Responsive Templates Generation</p>
+								<a href="https://w3layouts.com/free-responsive-html5-css3-website-templates/" target="_blank" class="btn button-style mt-sm-5 mt-4">Browse All</a>
+							</div>
+						</div>
+					</li>
+					<li>
+						<div class="container">
+							<div class="w3ls_banner_txt text-center">
+								<h3 class="w3ls_pvt-title text-wh text-uppercase let">W3layouts Website</h3>
+								<p class="text-li">New HTML Responsive Templates Generation</p>
+								<a href="https://w3layouts.com/free-responsive-html5-css3-website-templates/" target="_blank" class="btn button-style mt-sm-5 mt-4">Browse All</a>
+							</div>
+						</div>
+					</li>
+					<li>
+						<div class="container">
+							<div class="w3ls_banner_txt text-center">
+								<h3 class="w3ls_pvt-title text-wh text-uppercase let">W3layouts Website</h3>
+								<p class="text-bl">New HTML Responsive Templates Generation</p>
+								<a href="https://w3layouts.com/free-responsive-html5-css3-website-templates/" target="_blank" class="btn button-style mt-sm-5 mt-4">Browse All</a>
+							</div>
+						</div>
+					</li>
+				</ul>
+				<div class="arrows">
+					<label for="slides_1"></label>
+					<label for="slides_2"></label>
+					<label for="slides_3"></label>
+				</div>
+			</div>
+		</div>
+		<!-- //banner -->
+	</div>
+	<!-- //main banner -->
+
+	<!-- about-->
+	<section class="single_grid_w3_main align-w3 py-5" id="about">
+		<div class="container py-xl-5 py-lg-3">
+			<div class="wthree_pvt_title text-center">
+				<h4 class="w3pvt-title">About Us</h4>
+				<p class="sub-title">This is a demo text you may write a brief text here to explain the title</p>
+			</div>
+			<div class="row pt-lg-4">
+				<div class="col-lg-6">
+					<div class="single_grid_w3 single_grid_w3">
+						<img src="images/s1.jpg" alt="" class="img-fluid" />
+					</div>
+				</div>
+				<div class="col-lg-6">
+					<div class="single_grid_text">
+						<h5>sub heading goes here</h5>
+						<hr>
+						<p>Subheadings stand out because they are like mini titles. They make your post stand out and
+							make it more readable.</p>
+						<a class="btn mt-sm-5 mt-4 button-style-2" href="about.html">View More</a>
+					</div>
+				</div>
+			</div>
+			<div class="row flex-row-reverse sec-space">
+				<div class="col-lg-6 pr-lg-0 clo-res">
+					<div class="single_grid_w3 single_grid_w31">
+						<img src="images/s2.jpg" alt="" class="img-fluid" />
+					</div>
+				</div>
+				<div class="col-lg-6 pl-lg-0 clo-res">
+					<div class="single_grid_text">
+						<h5>sub heading goes here</h5>
+						<hr>
+						<p>Subheadings stand out because they are like mini titles. They make your post stand out and
+							make it more readable.</p>
+						<a class="btn mt-sm-5 mt-4 button-style-2" href="about.html">View More</a>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-6">
+					<div class="single_grid_w3 single_grid_w32">
+						<img src="images/s3.jpg" alt="" class="img-fluid" />
+					</div>
+				</div>
+				<div class="col-lg-6">
+					<div class="single_grid_text">
+						<h5>sub heading goes here</h5>
+						<hr>
+						<p>Subheadings stand out because they are like mini titles. They make your post stand out and
+							make it more readable.</p>
+						<a class="btn mt-sm-5 mt-4 button-style-2" href="about.html">View More</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- //about -->
+
+	<!-- services -->
+	<section class="services bg-li py-5" id="services">
+		<div class="container py-xl-5 py-3">
+			<div class="row service-grids">
+				<div class="serv-grid-left col-lg-4">
+					<h4 class="mb-2">What we offer</h4>
+					<h3 class="heading mb-3">We Provide Awesome Services</h3>
+					<p>Aac finibus ante. Sed mattis risused ipsum dolor vivamus eget est int sodio tempor interdum et. Mauris maximus
+						fermentume arcu, acne finibus ante. Sed mattis risus at ipsum elementum, sed auctor turpis cursus risus at ipsum
+						vivamus.</p>
+				</div>
+				<div class="col-lg-8 mt-lg-0 mt-5">
+					<div class="row process-grids">
+						<div class="col-md-6">
+							<div class="ser-grd">
+								<div class="row">
+									<div class="col-md-3 col-2 text-center services-icon icon-clr1">
+										<span class="fa fa-paint-brush"></span>
+									</div>
+									<div class="col-md-9 col-10 text-left services-grid">
+										<h4>ipsum fdd dolor</h4>
+										<p>Natus error sit voluptatem accusantium dolo remque lauda ntium.</p>
+									</div>
+								</div>
+							</div>
+							<div class="ser-grd mt-md-5 mt-4">
+								<div class="row">
+									<div class="col-md-3 col-2 text-center services-icon icon-clr2">
+										<span class="fa fa-coffee"></span>
+									</div>
+									<div class="col-md-9 col-10 text-left services-grid">
+										<h4>sit amet conse</h4>
+										<p>Natus error sit voluptatem accusantium dolo remque lauda ntium.</p>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-6">
+							<div class="ser-grd mt-md-0 mt-4">
+								<div class="row">
+									<div class="col-md-3 col-2 text-center services-icon icon-clr3">
+										<span class="fa fa-pencil"></span>
+									</div>
+									<div class="col-md-9 col-10 text-left services-grid">
+										<h4>cte turad ipis</h4>
+										<p>Natus error sit voluptatem accusantium dolo remque lauda ntium.</p>
+									</div>
+								</div>
+							</div>
+							<div class="ser-grd mt-md-5 mt-4">
+								<div class="row">
+									<div class="col-md-3 col-2 text-center services-icon icon-clr4">
+										<span class="fa fa-lightbulb-o"></span>
+									</div>
+									<div class="col-md-9 col-10 text-left services-grid">
+										<h4>sed do eius</h4>
+										<p>Natus error sit voluptatem accusantium dolo remque lauda ntium.</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- //services -->
+
+	<!-- testimonials -->
+	<section class="testimonials text-center py-5" id="testi">
+		<div class="container py-xl-5 py-lg-3">
+			<div class="py-xl-4 py-lg-2">
+				<img src="images/testi2.jpg" alt="" class="img-fluid test-img" />
+				<h3 class="mt-4 mb-2">Jenifer Burns</h3>
+				<ul class="list-unstyled w3ls-icons clients">
+					<li>
+						<a href="#">
+							<span class="fa fa-star"></span>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<span class="fa fa-star"></span>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<span class="fa fa-star"></span>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<span class="fa fa-star"></span>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<span class="fa fa-star-half-o"></span>
+						</a>
+					</li>
+				</ul>
+				<p class="testi-info text-wh mt-4 pt-3"><span class="fa fa-quote-left mr-2"></span>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud consectetur incididunt
+					ut.. <span class="fa fa-quote-right ml-2"></span>
+				</p>
+			</div>
+		</div>
+	</section>
+	<!-- //testimonials -->
+
+	<!-- subscribe -->
+	<section class="subscribe py-lg-5 py-4" id="sub">
+		<div class="container pt-5">
+			<div class="row">
+				<div class="col-lg-9 mx-auto">
+					<h5 class="main-title-w3pvt text-center">subscribe to my newsletter</h5>
+					<form action="#" method="post" class="subscribe-wthree">
+						<div class="d-flex subscribe-wthree-field">
+							<label class="align-self-center">
+								<span class="fa fa-envelope-open" aria-hidden="true"></span>
+							</label>
+							<input class="form-control" type="email" placeholder="Enter your email..." name="email" required="">
+							<button class="btn form-control w-50" type="submit">Subscribe</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- //subscribe -->
+
+	<!-- sponsors -->
+	<div class="spons text-center py-5">
+		<div class="container py-xl-5 py-lg-3">
+			<div class="row">
+				<div class="col-md-2 col-sm-4 col-6 spons-one">
+					<img src="images/sp1.png" alt="" class="img-fluid" />
+					<p>Sed Dos</p>
+				</div>
+				<div class="col-md-2 col-sm-4 col-6 spons-one">
+					<img src="images/sp2.png" alt="" class="img-fluid" />
+					<p>eiu smod</p>
+				</div>
+				<div class="col-md-2 col-sm-4 col-6 spons-one mt-sm-0 mt-3">
+					<img src="images/sp3.png" alt="" class="img-fluid" />
+					<p>ut lab</p>
+				</div>
+				<div class="col-md-2 col-sm-4 col-6 spons-one mt-md-0 mt-3">
+					<img src="images/sp4.png" alt="" class="img-fluid" />
+					<p>ore etdo</p>
+				</div>
+				<div class="col-md-2 col-sm-4 col-6 spons-one mt-md-0 mt-3">
+					<img src="images/sp5.png" alt="" class="img-fluid" />
+					<p>lore ma</p>
+				</div>
+				<div class="col-md-2 col-sm-4 col-6 spons-one mt-md-0 mt-3">
+					<img src="images/sp6.png" alt="" class="img-fluid" />
+					<p>na aliqu</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- //sponsors -->
+
+	<!-- footer -->
+	<footer class="py-sm-5 py-4">
+		<div class="container-fluid px-sm-5 py-xl-4">
+			<div class="row footer-top">
+				<div class="col-lg-4 col-md-6 footer-grid_section_1its">
+					<h2 class="logo-2 mb-lg-4 mb-3">
+						<a href="index.html">Draw</a>
+					</h2>
+					<!-- social icons -->
+					<div class="mobamuinfo_social_icons">
+						<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+						<h4 class="sub-con-fo mt-4 mb-3">Catch on Social</h4>
+						<ul class="mobamuits_social_list list-unstyled">
+							<li class="w3_mobamu_facebook">
+								<a href="#">
+									<span class="fa fa-facebook-f"></span>
+								</a>
+							</li>
+							<li class="w3_mobamu_twitter">
+								<a href="#">
+									<span class="fa fa-twitter"></span>
+								</a>
+							</li>
+							<li class="w3_mobamu_dribble">
+								<a href="#">
+									<span class="fa fa-dribbble"></span>
+								</a>
+							</li>
+							<li class="w3_mobamu_google">
+								<a href="#">
+									<span class="fa fa-google-plus"></span>
+								</a>
+							</li>
+						</ul>
+						<!-- social icons -->
+					</div>
+				</div>
+				<div class="col-lg-2 col-md-6 footer-grid_section_1its mt-md-0 mt-5">
+					<h3 class="footer-title mb-lg-4 mb-3">Navigation</h3>
+					<ul class="list-unstyled">
+						<li class="mt-3">
+							<a href="index.html">Home</a>
+						</li>
+						<li class="mt-3">
+							<a href="about.html">About Us</a>
+						</li>
+						<li class="mt-3">
+							<a href="gallery.html">Gallery</a>
+						</li>
+						<li class="mt-3">
+							<a href="#services">Services</a>
+						</li>
+						<li class="mt-3">
+							<a href="contact.html">Contact Us</a>
+						</li>
+					</ul>
+				</div>
+				<div class="col-lg-3 col-md-6 footer-grid_section_1its mt-lg-0 mt-5">
+					<h3 class="footer-title mb-lg-4 mb-3">Recent Comments</h3>
+					<div class="contact-info">
+						<div class="footer-style-w3ls mt-4">
+							<p class="font-italic">Petey Cruiser</p>
+							<span class="mb-2 let">Sed do eiusmod tempor labore</span>
+						</div>
+						<div class="footer-style-w3ls mt-4">
+							<p class="font-italic">Anna Sthesia</p>
+							<span class="mb-2 let">Lorem ipsum dolor sit amet</span>
+						</div>
+						<div class="footer-style-w3ls mt-4">
+							<p class="font-italic">Bob Frapples</p>
+							<span class="mb-2 let">Sed do eiusm odor ut labore eius mod tempor.</span>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-6 footer-grid_section_1its mt-lg-0 mt-5">
+					<h3 class="footer-title mb-lg-4 mb-3">Contact Info</h3>
+					<div class="contact-info">
+						<div class="footer-style-w3ls mt-4">
+							<h4 class="mb-2">Phone</h4>
+							<p>+121 098 8907 9987</p>
+						</div>
+						<div class="footer-style-w3ls mt-4">
+							<h4 class="mb-2">Email </h4>
+							<p><a href="mailto:info@example.com">info@example.com</a></p>
+						</div>
+						<div class="footer-style-w3ls mt-4">
+							<h4 class="mb-2">Location</h4>
+							<p>Honey Avenue, New York City</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+	<!-- //footer -->
+	<!-- copyright -->
+	<div class="cpy-right text-center py-4">
+		<p>© 2019 Draw. All rights reserved | Design by
+			<a href="http://w3layouts.com"> W3layouts.</a>
+		</p>
+	</div>
+	<!-- //copyright -->
+	<!-- move top icon -->
+	<a href="#home" class="move-top text-center"></a>
+	<!-- //move top icon -->
+
+</body>
+
+</html>
